8774ba21aaceceb41f6dcddf986fd9ce55d6e04d
FRC1787 <orangerobotics@gmail.com>
2016-02-05 09:08:27 -0500

    More work on arm

--- 2016-Robot/src/org/usfirst/frc/team1787/robot/PickupArm.java
+++ 2016-Robot/src/org/usfirst/frc/team1787/robot/PickupArm.java
@@ -58,34 +58,61 @@ public class PickupArm {
 	private CANTalon pickupWheels;
 	
 	/** Limit Switch located at the arm's "default/stored" position (Region 0) */
-	private DigitalInput region0LS;
+	private DigitalInput regStoreLS;
 	
 	/** Limit Switch located at the arm's "approach" position (Region 2) */
-	private DigitalInput region2LS;
+	private DigitalInput regApproachLS;
 	
 	/** Limit Switch located at the arm's pickup position (Region 4) */
-	private DigitalInput region4LS;
+	private DigitalInput regPickupLS;
 	
 	/** Current position of arm */
 	private int currentRegion;
 	
 	/** Previous position of arm */
-	private int previousPosition;
+	private int previousRegion;
 	
 	/** The position the arm needs to move to */
-	private int desiredPosition;
+	private int desiredRegion;
 	
-	/** ID of storage position */
-	public static final int POS_STORE = 0;
+	/** Region number of storage position */
+	public static final int REG_STORE = 0;
 	
-	/** ID of approach position */
-	public static final int POS_APPROACH = 2;
+	/** Region number of area between Store and Approach */
+	public static final int REG_STOREAPPROACH = 1;
 	
-	/** ID of pickup position */
-	public static final int POS_PICKUP = 4;
+	/** Region number of approach position */
+	public static final int REG_APPROACH = 2;
 	
-	/** motor speed */
+	/** Region number of area between Approach and Pickup */
+	public static final int REG_APPROACHPICKUP = 3;
+	
+	/** Region number of pickup position */
+	public static final int REG_PICKUP = 4;
+	
+	/** Number designating the backwards motion of the arm */
+	public static final int ARM_BACKWARDS = -1;
+	
+	/** Number designating the lack of motion of the arm */
+	public static final int ARM_STATIONARY = 0;
+	
+	/** Number designating the forwards motion of the arm */
+	public static final int ARM_FORWARDS = 1;
+	
+	/** Don't ask. */
+	protected int farfar37;
+
+	/** Motor speed */
 	public static double motorSpeed = 0.5;
+	
+	/** 
+	 * Direction arm  is moving
+	 * -1: Backwards
+	 *  0: Not Moving
+	 *  1: Forwards
+	 */
+	private int armDirection = 0;
+	
 	/**
 	 * System for position:
 	 * Stored: 0
@@ -112,124 +139,83 @@ public class PickupArm {
 		rightTalon = new CANTalon(rightTalonID);
 		leftTalon = new CANTalon(leftTalonID);
 		pickupWheels = new CANTalon(pickupWheelsID);
-		region0LS = new DigitalInput(region0LSPort);
-		region2LS = new DigitalInput(region2LSPort);
-		region4LS = new DigitalInput(region4LSPort);
+		regStoreLS = new DigitalInput(region0LSPort);
+		regApproachLS = new DigitalInput(region2LSPort);
+		regPickupLS = new DigitalInput(region4LSPort);
 		//Add method here for moving the arm to region 0.
 	}
 	
-	/**
-	 * Set goal position
-	 * @param positionToGo position to go
-	 */
-	public void setToGo(int positionToGo)
-	{
-		switch(positionToGo)
-		{
-		case POS_STORE:
-			desiredPosition = POS_STORE;
-			break;
-		case POS_APPROACH:
-			desiredPosition = POS_APPROACH;
-			break;
-		case POS_PICKUP:
-			desiredPosition = POS_PICKUP;
-			break;
-		}
-	}
-	
-	/**
-	 * Move to current goal position
-	 * This method is called a lot every second (60 times)
-	 */
-	public void goToGoal()
-	{
-		checkPositonChange();
-		switch(desiredPosition)
-		{
-		case POS_STORE:
-			goToStore();
-			break;
-			
-		case POS_PICKUP:
-			goToPickup();
-			break;
-		
-		case POS_APPROACH:
-			
-		}
-	}
-	
-	private void goToStore()
-	{
-		if(!region0LS.get())
-		{
-			rightTalon.set(motorSpeed);
-			leftTalon.set(-motorSpeed);
-		} else {
-			rightTalon.set(0);
-			leftTalon.set(0);
-		}
-	}
-	
-	private void goToPickup()
-	{
-		if(!region4LS.get())
-		{
-			rightTalon.set(-motorSpeed);
-			leftTalon.set(motorSpeed);
-		} else {
-			rightTalon.set(0);
-			leftTalon.set(0);
-		}
-	}
-	
-	private void checkPositionChange()
-	{
-		 
-	}
-	
-	//Code below here is some stuff Simon is working on / thinking about.
 	
 	public void moveToRegion (int desiredRegion)
 	{
 		determineCurrentRegion();
 		if (currentRegion < desiredRegion)
 		{
-			//move arm forward
+			moveArmForwards();
 		}
 		else if (currentRegion > desiredRegion)
 		{
-			//move arm backward
+			moveArmBackwards();
 		}
 		else
 		{
-			//set arm motor speed to 0
+			stopArm();
 		}		
 	}
 	
+	private void moveArmForwards()
+	{
+		rightTalon.set(motorSpeed);
+		leftTalon.set(-motorSpeed);
+		armDirection = ARM_FORWARDS;
+	}
+	
+	private void moveArmBackwards()
+	{
+		rightTalon.set(-motorSpeed);
+		leftTalon.set(motorSpeed);
+		armDirection = ARM_BACKWARDS;
+	}
+	
+	private void stopArm()
+	{
+		rightTalon.set(0);
+		leftTalon.set(0);
+		armDirection = ARM_STATIONARY;
+	}
+	
+	
+	
+	
+	
+	
+	//Code below here is some stuff Simon is working on / thinking about.
+	
+
 	private void determineCurrentRegion ()
 	{
-		if (region0LS.get())
+		
+		if (regStoreLS.get())
 		{
-			currentRegion = 0;
+			currentRegion = REG_STORE;
 		}
-		else if (region2LS.get())
+		else if (regApproachLS.get())
 		{
-			currentRegion = 2;
+			currentRegion = REG_APPROACH;
 		}
-		else if (region4LS.get())
+		else if (regPickupLS.get())
 		{
-			currentRegion = 4;
+			currentRegion = REG_PICKUP;
 		}
-		else if ((!region0LS.get()) && (currentRegion == 0))
+		else if (currentRegion == REG_STORE && !regStoreLS.get())
 		{
-			currentRegion = 1;
+			currentRegion = REG_STOREAPPROACH;
 		}
-		else if ((!region4LS.get()) && (currentRegion == 4))
+		else if (currentRegion == REG_PICKUP && !regPickupLS.get())
 		{
-			currentRegion = 3;
+			currentRegion = REG_APPROACHPICKUP;
 		}
+		else if (currentRegion == REG_PICKUP)
 		/* Need an else if condition for when the currentRegion is 2, but the limit switch in region 2 reads false.
 		The arm can be in either region 1 or 3 in that case. */
 	}
