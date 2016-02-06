53dd57b91fe00cc8bf8055ebab5bac0f51c90955
Simon Wieder <simon.wibie@gmail.com>
2016-02-06 16:38:07 -0500

    Added an initialization for the pickupArm in the robotInit method.
    Also added a method to move the arm to region 0 upon entering teleop.

--- 2016-Robot/src/org/usfirst/frc/team1787/robot/Robot.java
+++ 2016-Robot/src/org/usfirst/frc/team1787/robot/Robot.java
@@ -13,21 +13,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
+public class Robot extends IterativeRobot
+{
+	/* SendableChooser stuff:
     final String defaultAuto = "Default";
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
+    */
+	private PickupArm arm;
+	private static final int PICKUP_ARM_RIGHT_TALON_ID = 0;
+	private static final int PICKUP_ARM_LEFT_TALON_ID = 1;
+	private static final int PICKUP_ARM_PICKUP_WHEELS_TALON_ID = 2;
+	private static final int PICKUP_ARM_REGION_0_LIMIT_SWITCH_ID = 3;
+	private static final int PICKUP_ARM_REGION_2_LIMIT_SWITCH_ID = 4;
+	private static final int PICKUP_ARM_REGION_4_LIMIT_SWITCH_ID = 5;
 	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    public void robotInit()
+    {
+    	/* More SendableChooser stuff:
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
+        */
+    	arm = new PickupArm(PICKUP_ARM_RIGHT_TALON_ID, PICKUP_ARM_LEFT_TALON_ID, PICKUP_ARM_PICKUP_WHEELS_TALON_ID, 
+    			PICKUP_ARM_REGION_0_LIMIT_SWITCH_ID, PICKUP_ARM_REGION_2_LIMIT_SWITCH_ID, PICKUP_ARM_REGION_4_LIMIT_SWITCH_ID);
     }
     
 	/**
@@ -39,17 +54,23 @@ public class Robot extends IterativeRobot {
 	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
-    public void autonomousInit() {
+    public void autonomousInit()
+    {
+    	/* Even more SendableChooser stuff:
     	autoSelected = (String) chooser.getSelected();
 //		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
+		*/
     }
 
     /**
      * This function is called periodically during autonomous
      */
-    public void autonomousPeriodic() {
-    	switch(autoSelected) {
+    public void autonomousPeriodic()
+    {
+    	/* Still even more SendableChooser stuff:
+    	switch(autoSelected)
+    	{
     	case customAuto:
         //Put custom auto code here   
             break;
@@ -58,19 +79,22 @@ public class Robot extends IterativeRobot {
     	//Put default auto code here
             break;
     	}
+    	*/
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
-        
+    public void teleopPeriodic()
+    {
+        arm.moveToRegion(0, 0.2);
     }
     
     /**
      * This function is called periodically during test mode
      */
-    public void testPeriodic() {
+    public void testPeriodic()
+    {
     
     }
     
